eef688d3955ed9b4c5d3dee8b5d2e758d124f87f
Georgi Dimitrov <Themikuma@gmail.com>
2022-01-24 15:07:16 +0100

    Implemented SumAll function.
    SumAll allows the user to input multiple collections and receive a single collection containing the sums of each line.

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,3 +8,12 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+//SumAll sums all the elements in multiple slices of arrays
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,19 +1,47 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestSum(t *testing.T) {
 
 	t.Run("Sum collection of 5 elements", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 5, 8}
 
 		got := Sum(numbers)
-		want := 15
+		want := 19
 
 		if got != want {
-			t.Errorf("want %q got %q given %v", want, got, numbers)
+			t.Errorf("want %d got %d given %v", want, got, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Helper()
+	var formattedAssert = func(t testing.TB, got []int, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %v got %v", want, got)
+		}
+	}
+	
+	t.Run("multiple collections with elements", func(t *testing.T) {
+		want := []int{20, 10}
+		got := SumAll([]int{4, 6, 10}, []int{2, 3, 5})
+		formattedAssert(t, got, want)
+	})
+
+	t.Run("multiple collections with no elements", func(t *testing.T) {
+		want := []int{0, 0}
+		got := SumAll([]int{}, []int{})
+		formattedAssert(t, got, want)
+	})
+
+	t.Run("single collection with no elements", func(t *testing.T) {
+		want := []int{0}
+		got := SumAll([]int{})
+		formattedAssert(t, got, want)
 	})
 }
