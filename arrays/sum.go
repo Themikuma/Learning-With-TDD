c9afaced1b8d94d4907ebdbc86353d6426b6dc3d
Georgi Dimitrov <Themikuma@gmail.com>
2022-01-24 17:38:58 +0100

    Implemented SumAllTails
    Adopted a patter of table tests for the SumAllTails function.

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,6 @@
 package arrays
 
-//Sum returns the sum of the elements of the array
+//Sum returns the sum of the elements of the given slice
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
@@ -9,11 +9,26 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-//SumAll sums all the elements in multiple slices of arrays
+//SumAll sums all the elements in multiple slices
 func SumAll(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, number := range numbersToSum {
 		sums = append(sums, Sum(number))
+	}
+	return sums
+}
+
+//SumAllTails sums all the tails in multiple slices
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum((tail)))
+
+		}
 	}
 	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -19,29 +19,64 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumAll(t *testing.T) {
+func arrayAssertHelper(t *testing.T, want []int, got []int) {
 	t.Helper()
-	var formattedAssert = func(t testing.TB, got []int, want []int) {
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %v got %v", want, got)
-		}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("want %v got %v", want, got)
 	}
-	
+}
+
+func TestSumAll(t *testing.T) {
+
 	t.Run("multiple collections with elements", func(t *testing.T) {
 		want := []int{20, 10}
 		got := SumAll([]int{4, 6, 10}, []int{2, 3, 5})
-		formattedAssert(t, got, want)
+		arrayAssertHelper(t, got, want)
 	})
 
 	t.Run("multiple collections with no elements", func(t *testing.T) {
 		want := []int{0, 0}
 		got := SumAll([]int{}, []int{})
-		formattedAssert(t, got, want)
+		arrayAssertHelper(t, got, want)
 	})
 
 	t.Run("single collection with no elements", func(t *testing.T) {
 		want := []int{0}
 		got := SumAll([]int{})
-		formattedAssert(t, got, want)
+		arrayAssertHelper(t, got, want)
 	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	tests := []struct {
+		name         string
+		numbersToSum [][]int
+		want         []int
+	}{
+		{
+			name: "multiple collections with elements",
+			numbersToSum: [][]int{
+				{3, 1, 2},
+				{15, 2, 3}},
+			want: []int{3, 5}},
+		{
+			name: "multiple collections with no elements",
+			numbersToSum: [][]int{
+				{},
+				{}},
+			want: []int{0, 0}},
+		{
+			name: "single collection with single element",
+			numbersToSum: [][]int{
+				{42}},
+			want: []int{0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SumAllTails(tt.numbersToSum...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SumAllTails() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
