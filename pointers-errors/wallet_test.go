2b78c45ed159c594a42722d3a011ae88b5e8a44d
Georgi Dimitrov <themikuma@gmail.com>
2022-02-16 11:58:01 +0200

    Created a wallet with deposit and withdraw functionalities.

---
+++ pointers-errors/wallet_test.go
@@ -0,0 +1,37 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: 15}
+		wallet.Withdraw(Bitcoin(15))
+		assertBalance(t, wallet, Bitcoin(0))
+	})
+
+	t.Run("Withdraw over the limit", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+		assertBalance(t, wallet, startingBalance)
+		if err == nil {
+			t.Error("Wanted error but got none")
+		}
+	})
+}
